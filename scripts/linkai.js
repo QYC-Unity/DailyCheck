624d4638b4d4823bc0f4c3378eb0c9746a17cb45
轻尘 <764725293@qq.com>
2023-12-19 14:00:24 +0800

    fix bug

--- scripts/linkai.js
+++ scripts/linkai.js
@@ -1,26 +1,26 @@
 // https://chat.link-ai.tech/home?share=GBoWyH
- const axios = require("axios");
+const axios = require("axios");
 function linkai() { 
   return new Promise(async (resolve) => {
     try {
       const url = 'https://chat.link-ai.tech/api/chat/web/app/user/sign/in';
-  const token = config.linkai.Authorization; // 
+      const token = config.linkai.Authorization; // 
   
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: token,
-      },
-    });
+      const response = await axios.get(url, {
+        headers: {
+          Authorization: token,
+        },
+      });
 
-    if (response.data.success) {
-      msg = `签到成功，获得 ${response.data.data.score} 积分`;
-    } else {
-      msg = `签到失败，原因：${response.data.message}`;
-    }
-    console.log(msg)
+      if (response.data.success) {
+        msg = `签到成功，获得 ${response.data.data.score} 积分`;
+      } else {
+        msg = `签到失败，原因：${response.data.message}`;
+      }
     } catch (error) {
       msg =`签到失败，原因：${error.message}`;
     }
+    console.log(msg)
     resolve("【linkai】：" + msg);
   });
 }
